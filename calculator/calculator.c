cf25480b819cbf471c8deef8e277d76cc45a26e1
jacob120802 <86815451+jacob120802@users.noreply.github.com>
2021-07-02 17:19:50 +0530

    Add files via upload

---
+++ calculator/calculator.c
@@ -0,0 +1,75 @@
+//C Program to simulate a simple calculator
+#include <stdio.h>
+#include<windows.h>
+
+void addition();
+void subtraction();
+void multiply();
+void divide();
+
+int main()
+{
+    int opt;
+    system("color 02");
+    printf("\t***MENU***\n\n1-Addition\n2-Substraction\n3-Multiplication\n4-Division\n5-Exit\n");
+    printf("\tEnter your choice : ");
+    scanf("%d",&opt);
+    switch(opt)
+    {
+      case 1:
+        addition();
+        break;
+        
+      case 2:
+        subtraction();
+        break;
+        
+      case 3:
+        multiply();
+        break;  
+      
+      case 4:
+        divide();
+        break;
+        
+      case 5: 
+        break; 
+        
+      default:
+        printf("Invalid Input\n");
+        break; 
+    }
+    return 0;
+}
+void addition()
+{
+  float a,b;
+  printf("Enter two numbers\n");
+  scanf("%f%f",&a,&b);
+  printf("Sum of %.2f and %.2f is %.2f",a,b,a+b);
+}
+
+void subtraction()
+{
+  float a,b;
+  printf("Enter two numbers\n");
+  scanf("%f%f",&a,&b);
+  printf("subtraction of %.2f and %.2f is %.2f",a,b,a-b);
+}
+
+void multiply()
+{
+  float a,b;
+  printf("Enter two numbers\n");
+  scanf("%f%f",&a,&b);
+  printf("multiplication of %.2f and %.2f is %.2f",a,b,a*b);
+}
+
+void divide()
+{
+  float a,b;
+  printf("Enter two numbers\n");
+  scanf("%f%f",&a,&b);
+  printf("division of %.2f and %.2f is %.2f",a,b,a/b);
+}
+
